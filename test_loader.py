0429db3a307432092e465c79df224a1cb0016f67
Tomas Lovato <tomas.lovato@cmcc.it>
2020-08-18 16:20:12 +0200

    dummy tester

--- test_loader.py
+++ test_loader.py
@@ -19,12 +19,17 @@ from geocat.viz import util as gvutil
 # check catalogue contents
 zdat.inquire_catalogue()
 
+zdat.inquire_catalogue(dataset='ERA5_MM', info=True)
 # Read data and compose zonal means (local)
-xx=zdat.read_xarray(dataset='C-GLORSv7', var='votemper', period=[2000, 2001], season='JFM', level=500.)
+#xx=zdat.read_xarray(dataset='C-GLORSv7', var='votemper', period=[2000, 2001], season='JFM', level=500.)
 
 
+#ud=zcom.zonal_var('ERA5_MM','U','DJF')
+
+
+yy = zdat.read_xarray(dataset='ERA5_MM', var='SST', period=[2000, 2001], season='JFM', level=['SURF',])
 # Read data and compose zonal means (local)
-#ud=zcom.zonal_var('ERA5_MM','T', period=[2000, 2001], season='DJF',level=[100,])
+ud=zcom.zonal_var('ERA5_MM','T', period=[2000, 2001], season='DJF',level=[100,], option='Time')
 
 
 print('Done')
