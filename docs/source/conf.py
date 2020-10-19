c21c930e2b07b201dea490420845cb081c44bdf7
Tomas Lovato <tomas.lovato@cmcc.it>
2020-10-19 10:24:20 +0200

    set documentation version to 1.0 in docs/source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,9 +29,9 @@ copyright = '2020, AN'
 author = 'AN'
 
 # The short X.Y version
-version = '0.1'
+version = '1.0'
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '1.0'
 
 
 # -- General configuration ---------------------------------------------------
